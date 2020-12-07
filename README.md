07c05a772341020ddc75ab610b97ebb1665ad3a8
Akihito Koriyama <akihito.koriyama@gmail.com>
2020-12-07 14:15:32 +0900

    Init commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# asd-sync
+
+A tool for rendering the ALPS application diagram in real time.
+
+## Requirement
+
+ * npm
+ * asd
+
+## Installation
+
+```
+npm install
+```
+
+## Run
+
+```
+gulp
+```
---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync');
+const { execSync }  = require('child_process');
+
+function serve(cb) {
+    browserSync({
+        server: {
+            baseDir: 'profile',
+        },
+        ghostMode: false,
+        open: 'external',
+        notify: true,
+    });
+    cb();
+}
+
+function reload(cb) {
+    browserSync.reload();
+    cb();
+}
+
+function asd(cb) {
+    cmd = 'asd ' + __dirname + '/profile/alps.json';
+    try {
+        return execSync(cmd).toString();
+    } catch (error) {
+        console.error(error.message);
+    }
+    cb();
+}
+
+function watch() {
+    gulp.watch('profile/alps.json', gulp.series(asd, reload));
+}
+
+exports.default = gulp.series(
+    asd,
+    gulp.parallel(serve, watch)
+);
